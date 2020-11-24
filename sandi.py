b74c259eae7605b90618ec7c7cd904489fdd6c72
Teyak <74811879+Teyak@users.noreply.github.com>
2020-11-24 19:49:14 +0700

    Create sandi.py

---
+++ sandi.py
@@ -0,0 +1,33 @@
+import os, sys
+print ("\033[32;1m{Masukan username & password}")
+print ("\033[32;1m{mohon masukan user & pw nya dengan benar}")
+username = 'TEYAK'
+password = 'TEAM'
+
+def restart():
+	ngulang = sys.executable
+	os.execl(ngulang, ngulang, *sys.argv)
+
+def main():
+	uname = raw_input("username : ")
+	if uname == username:
+		pwd = raw_input("password : ")
+
+		if pwd == password: 
+			sys.exit()
+
+		else:
+			print "\n\033[1;36mSorry Invalid Password !!!\033[00m"
+			print "Back Login\n"
+			restart()
+
+	else:
+		print "\n\033[1;36mSorry Invalid Username !!!\033[00m"
+		print "Back Login\n"
+		restart()
+
+try:
+	main()
+except KeyboardInterrupt:
+	os.system('clear')
+	restart()
